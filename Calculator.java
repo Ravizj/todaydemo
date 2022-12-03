074c09598d8dbd1132afdee336d177b05413a0d7
Ravi Kumar <110003783+Ravizj@users.noreply.github.com>
2022-12-03 19:49:41 +0530

    first commit

---
+++ Calculator.java
@@ -0,0 +1,21 @@
+package com.bridgelabz.basics;
+
+public class Calculator {
+//ravi kumar
+    static void sum(int num1,int num2){
+        int sum = num1 + num2;
+        System.out.println(sum);
+    }
+
+    static int multiply(int num1,int num2){
+        int product = num1*num2;
+        return product;
+    }
+
+    public static void main(String[] args) {
+        sum(3,7);
+        int result = multiply(4,8);
+        System.out.println(result);
+
+    }
+}
